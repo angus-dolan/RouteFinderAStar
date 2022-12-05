e9aaf5a85e4869421678c194b0b87c72cc68072e
Angus Dolan <angus@angusdolan.com>
2022-12-05 20:10:02 +0000

    Adds command line args handling

--- RouteFinderAStar/Program.cs
+++ RouteFinderAStar/Program.cs
@@ -1,1 +1,27 @@
-﻿
+﻿using System.IO;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        if (args.Length == 1)
+        {
+            try
+            {
+                string filepath = Directory.GetCurrentDirectory() + @"\" + args[0] + ".cav";
+                string file = File.ReadAllText(filepath);
+
+                BuildSolution(file);
+            } catch (FileNotFoundException) { }
+        }
+    }
+
+    static void BuildSolution(string file)
+    {
+        // Parse file contents
+
+        // Solve with A*, returns 0 when no route can be found
+
+        // Output results to .csn file
+    }
+}
